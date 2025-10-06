4fc6e4314a1ca82621cabb56f57131f153ab21d5
TinaSilver-fox <tinaisenka@gmail.com>
2025-10-06 23:24:07 +0300

    Add links

---
+++ Test/src/main/java/school/redrover/Main.java
@@ -0,0 +1,21 @@
+package school.redrover;
+
+public class Main {
+    public static void main(String[] args) {
+        // https://www.codewars.com/kata/545cedaa9943f7fe7b000048
+
+        // https://www.codewars.com/kata/55f8a9c06c018a0d6e000132
+
+        // https://www.codewars.com/kata/55fd2d567d94ac3bc9000064
+
+        // https://www.codewars.com/kata/515de9ae9dcfc28eb6000001
+
+        // https://www.codewars.com/kata/578553c3a1b8d5c40300037c
+
+        // https://www.codewars.com/kata/585d7d5adb20cf33cb000235
+
+    }
+
+
+
+}
