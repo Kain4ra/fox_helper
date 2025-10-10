848897c44a66a770415171739fc81cc35a51ffbe
Zinaida <zmileshin@gmail.com>
2025-10-10 12:55:09 -0400

    adding task1 solution

--- Test/src/main/java/school/redrover/Task1.java
+++ Test/src/main/java/school/redrover/Task1.java
@@ -7,6 +7,12 @@ import java.util.Set;
 public class Task1 {
     public static void main(String[] args) {
         String str = "The quick brown fox jumps over the lazy dog";
+        String str2 = "The quick Yellow fox jumps over the lazy COW";
+
+
+        System.out.println("Zina's: " + isPangramZina( str));
+        System.out.println("Zina's: " + isPangramZina( str2));
+
     }
 
     public static boolean checkKain4ra(String sentence) {
@@ -15,5 +21,24 @@ public class Task1 {
             if (Character.isLetter(symbol)) listCharacters.add(symbol);
         }
         return listCharacters.size() == 26;
+    }
+
+
+
+    public static boolean isPangramZina(String expression) {
+        StringBuilder sb = new StringBuilder();
+        boolean result = false;
+        for (char e : expression.toCharArray()) {
+            if (Character.isLetter(e)) {
+                e = Character.toLowerCase(e);
+                if (sb.indexOf(String.valueOf(e)) == -1) {
+                    sb.append(e);
+                }
+                if (sb.length() == 26) {
+                    result = true;
+                }
+            }
+        }
+        return result;
     }
 }
