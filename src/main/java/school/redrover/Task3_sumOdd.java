6c8058c0ffe913865229fb84aef7abede85d4154
TinaSilver-fox <tinaisenka@gmail.com>
2025-10-13 03:05:05 +0300

    Tests fix

--- src/main/java/school/redrover/Task3_sumOdd.java
+++ src/main/java/school/redrover/Task3_sumOdd.java
@@ -1,4 +1,14 @@
 package school.redrover;
 
 public class Task3_sumOdd {
+    public static void main(String[] args) {
+        rowSumOddNumbersTina(2);
+        rowSumOddNumbersTina(3);
+        rowSumOddNumbersTina(5);
+    }
+
+    //Простите, вот с этой задачей я накололась :D
+    public static int rowSumOddNumbersTina(int n) {
+        return n * n * n;
+    }
 }
