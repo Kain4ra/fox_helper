37f69d978c40f64dc187569884e9d11c1cce783e
Yuliya <52186267+Iuliia-b@users.noreply.github.com>
2025-10-14 01:07:22 +0300

    Added task3_sumOdd (#12)
    Co-authored-by: Yuliya Bezlatneva <bezlatneva@yandex.com>

--- src/main/java/school/redrover/Task3_sumOdd.java
+++ src/main/java/school/redrover/Task3_sumOdd.java
@@ -5,10 +5,23 @@ public class Task3_sumOdd {
         rowSumOddNumbersTina(2);
         rowSumOddNumbersTina(3);
         rowSumOddNumbersTina(5);
+
+        System.out.println("\n-------Yuliya-------");
+        System.out.println(rowSumOddNumbersYuliya(3));
+        System.out.println(rowSumOddNumbersYuliya(5));
+        System.out.println(rowSumOddNumbersYuliya(42));
     }
 
     //Простите, вот с этой задачей я накололась :D
     public static int rowSumOddNumbersTina(int n) {
         return n * n * n;
+    }
+
+    public static int rowSumOddNumbersYuliya(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += n * n;
+        }
+        return sum;
     }
 }
